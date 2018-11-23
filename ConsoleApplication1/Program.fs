52fe340cf7215e8d29c21652edd46df82650c6a9
Vadim <39588115+vnikifirov@users.noreply.github.com>
2018-11-23 15:57:18 +0300

    Add a few files

---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,26 @@
+﻿open FsXaml
+open ViewModule.FSharp
+open System.Windows
+open System
+
+type Main = XAML<"Main.xaml">
+
+type ViewModel() as vm =
+    inherit ViewModule.ViewModelBase()
+    let mutable show = false
+    member __.DependentCommand =
+        vm.Factory.CommandSyncChecked(
+            (fun _ -> MessageBox.Show "Dependent!" |> ignore),
+            fun _ -> show)
+    member __.MainCommand =
+        vm.Factory.CommandSync(fun _ ->
+            show <- true
+            // I would expect DependentCommand to become available after this next call.
+            vm.DependentCommand.RaiseCanExecuteChanged())
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =
+    let window = Main(DataContext = ViewModel())
+    let app = Application().Run(window)
+    app
